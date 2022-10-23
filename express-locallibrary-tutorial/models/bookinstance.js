f27da72fa5acc5be57e0b128bb245e8b09744ad0
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-23 17:25:28 +0900

    lunxon으로 날짜 포맷 지정

--- express-locallibrary-tutorial/models/bookinstance.js
+++ express-locallibrary-tutorial/models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -17,7 +18,12 @@ const BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
+  console.log("this: ", this);
   return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
