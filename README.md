607129316802b4b23fc914449ccb6a8ec87c8e96
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-03 23:25:45 +0900

    환경 변수명 변경

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. npm i 를 통해서 패키지 설치
 
-2. package.json과 같은 레벨에 .env 파일을 생성하고 다음을 저장: MONGODB_URL=\<your mongodb url\>
+2. package.json과 같은 레벨에 .env 파일을 생성하고 다음을 저장: MONGODB_URI=\<your mongodb url\>
 
 3. mongoDB에 데이터 생성을 테스트하기 위해서 다음 명령어 실행: node populatedb \<your mongodb url\> _\*참고: OS에 따라 url에 작은따옴표나 큰따옴표가 필요_
--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URL;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("connected", () => console.log("mongoDB connected..."));
