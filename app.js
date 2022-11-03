108d25d4b653d4a979f5506a29be9ed0c08d858b
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-03 22:44:22 +0900

    파일 정리 및 경로 이동

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
+const createError = require("http-errors");
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
+
+// Create the Express application object
 const app = express();
-const port = 3000;
 
-const wiki = require("./wiki.js");
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("connected", () => console.log("mongoDB connected..."));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const a_middleware_function = function (req, res, next) {
-  // Perform some operations
-  console.log("a_middleware_function");
-  next(); // Call next() so Express will call the next middleware function in the chain.
-};
-
-// Function added with use() for all routes and verbs
-// app.use(a_middleware_function);
-
-app.use("/wiki", a_middleware_function);
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
-app.use("/wiki", wiki);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
 
-// app.use(express.static("public"));
-app.use("/media", express.static("public"));
+app.use(compression()); // Compress all routes
+// Note: For a high-traffic website in production you wouldn't use this middleware.
+// Instead, you would use a reverse proxy like Nginx
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
+  next(createError(404));
 });
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
